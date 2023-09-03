f5917c7ee293d7a6efbd719dc3f895e89702d862
rayankermanshahani <rayankermanshahani@protonmail.com>
2023-09-03 12:06:07 -0400

    completed section 1.1.5's exercises

--- ch1/ex1-5.scm
+++ ch1/ex1-5.scm
@@ -4,8 +4,8 @@
 ;               faced with is using applicative- order evaluation or normal-order evaluation. 
 ; He defines the following two procedures:
 
-(define (p) (p))
-(define (test x y)
+(define (p) (p)) 
+(define (test x y) 
   (if (= x 0) 0 y))
 
 ; then he evaluated the expression:
@@ -13,3 +13,12 @@
 
 ; What behavior will Ben observe with an interpreter that uses applicative-order evaluation? 
 ; What behavior will he observe with an interpreter that uses normal-order evaluation?
+
+; --------
+; p: just returns the operand it's given
+; test: returns the first operand if the operand is equal to 0, 
+;       otherwise returns the second operand
+
+; an interpreter with applicative-order evaluation will not run properly as it will try to evaluate p, which is undefined, before it gets to the body of the test procedure.
+
+; an interpreter normal-order (aka "lazy") evaluation on the other hand will return 0 after running the same procedure because it will only have evaluated the first operand for the test procedure which happens to satisfy the 'if' expression's predicate. in this case, the interpreter has no need to evaluate the second operand (which is p) and therefore does not run into the problem of p being undefined
