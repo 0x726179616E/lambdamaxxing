7c3b8bf82c9689f814e64f1116d1b2a11f66c7c8
rayankermanshahani <rayankermanshahani@protonmail.com>
2023-09-23 16:20:55 -0400

    completed exercise 1.19 gd

--- ch1/sec1-2/ex1-19.scm
+++ ch1/sec1-2/ex1-19.scm
@@ -1,31 +1,8 @@
 (require scheme)
 
-; computing the fibonacci numbers in a logarithmic number of steps
+; complete this procedure which computes the fubonacci numbers in a logarithmic number of steps
 
-; Transformation T:
-; a <-- a + b
-; b <-- a
-
-; calling T over and over again `n` times, starting with 1 and 0, produces the pair Fib(n+1) & Fib(n)
-
-; fibonacci numbers are produced by applying T^n to the nth power of the transformation T, 
-; starting with the pair (1,0)
-
-; consider T to be the special case of p=0 and q=1 in a family of transofrmations Tpq where:
-; Tpq transforms the pair (a,b) according to:
-;   a <-- bq + aq + ap
-;   b <-- bp + aq
-; 
-; if Tpq is applied twice, the effect is the same as using a single transformation Tp'q' 
-; and compute p' and q' in terms of p and q 
-
-; first transformation:
-;   a <-- b1q + a1q + a1p
-;   b <-- b1p + a1q
-; second transformation:
-;   a <-- (b1p + a1q)q + (b1q + a1q + a1p)q + (b1q + a1q + a1p)p
-;   b <-- (b1p + a1q)p + (b1q + a1q + a1p)q
-
+(define (square n) (* n n))
 
 (define (even? n) 
   (= (remainder n 2) 0))
@@ -38,11 +15,15 @@
         ((even? count) 
          (fib-iter a
                    b
-                   ; compute p'
-                   ; compute q'
+                   (+ (square p) (square q)) ; compute p'
+                   (+ (* 2 p q) (square q))  ; compute q'
                    (/ count 2)))
-        (else (fib-iter (+ (* b q) (* a q) (* a p))
-                        (+ (* b p) (* a q))
+        (else (fib-iter (+ (* b q) 
+                           (* a q) 
+                           (* a p))
+                        (+ 
+                          (* b p) 
+                          (* a q))
                         p
                         q
                         (- count 1)))))
@@ -50,4 +31,6 @@
 (display (fib 5))
 (newline)
 (display (fib 8))
+(newline)
+(display (fib 17))
 (newline)
